41cdf59ebba49deb542636362c1d2a08c330844b
Giovani S Rodrigues <71795768+GiovaniRSantos@users.noreply.github.com>
2024-04-28 16:23:24 -0300

    criacao da entidade projects

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectsModule } from './projects/projects.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      synchronize: true,
+    }),
+
+    ProjectsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
-  imports: [ProjectsModule],
 })
 export class AppModule {}
--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -1,1 +1,43 @@
-export class Project {}
+import { Entity, PrimaryColumn, Column } from 'typeorm';
+
+export enum ProjectStatus {
+  Pending = 'pending',
+  Active = 'active',
+  Cancelled = 'cancelled',
+  Completed = 'completed',
+}
+
+@Entity()
+export class Project {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'datetime', nullable: true })
+  started_at: Date | null;
+
+  @Column({ type: 'datetime', nullable: true })
+  cancelled_at: Date | null;
+
+  @Column({ type: 'datetime', nullable: true })
+  forecast_at: Date | null;
+
+  @Column({ type: 'simple-enum' })
+  status: ProjectStatus;
+
+  constructor(
+    props: {
+      name: string;
+      description: string;
+      started_at?: Date | null;
+      cancelled_at?: Date | null;
+      forecast_at?: Date | null;
+    },
+    id?: string,
+  ) {
+    Object.assign(this, props);
+    this.id = id ?? crypto.randomUUID();
+  }
+}
